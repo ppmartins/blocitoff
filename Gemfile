068a3d899acbeaf0da1791ee160bfdc3dd10babc
Pedro <ppereiramartins@gmail.com>
2016-03-21 10:58:34 +0000

    Work in progress

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ end
 # #2
 group :development do
   gem 'sqlite3'
+  gem 'letter_opener'
 end
 
 group :development, :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,24 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :registerable, :confirmable
+
+  before_save { self.email = email.downcase }
+  before_create :generate_auth_token
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: EMAIL_REGEX }
+
+
+  def generate_auth_token
+    loop do
+      self.auth_token = SecureRandom.base64(64)
+      break unless User.find_by(auth_token: auth_token)
+    end
+  end
 end
